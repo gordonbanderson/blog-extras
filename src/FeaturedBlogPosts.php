71e8dc22c185d186d80e3c4e36c95ea5104731d8
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-09-11 21:53:28 +0100

    FIX: Featured posts

---
+++ src/FeaturedBlogPosts.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gordon
+ * Date: 10/09/18
+ * Time: 18:35
+ */
+
+namespace Suilven\Blog;
+
+
+
+use SilverStripe\Blog\Model\BlogPost;
+use SilverStripe\Core\Extension;
+
+class FeaturedBlogPosts extends Extension
+{
+    public function FeaturedPosts()
+    {
+        return BlogPost::get()->filter('Featured', true);
+    }
+
+}
